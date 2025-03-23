f118176f6601fd4a30bcd7bfc2c4d4ddc50e5a7e
enrerst <287076@student.pwr.edu.pl>
2025-03-23 18:35:15 +0100

    Update kalkulator konsolowy.cpp

--- kalkulator konsolowy.cpp
+++ kalkulator konsolowy.cpp
@@ -8,19 +8,19 @@ double mem = 0;
 
 bool mem_used = false;
 
-double add(double a, double b) { 
-	return a + b; 
+double add(double a, double b) {
+	return a + b;
 }
 
-double sub(double a, double b) { 
-	return a - b; 
+double sub(double a, double b) {
+	return a - b;
 }
 
-double tim(double a, double b) { 
-	return a * b; 
+double tim(double a, double b) {
+	return a * b;
 }
 
-double div(double a, double b){
+double div(double a, double b) {
 	if (b == 0) {
 		cout << "blad" << endl;
 		return 0;
@@ -28,11 +28,11 @@ double div(double a, double b){
 	return a / b;
 }
 
-double mod(double a, double b) { 
-	return a - (int)(a / b) * b; 
+double mod(double a, double b) {
+	return a - (int)(a / b) * b;
 }
 
-void erase(){
+void erase() {
 	mem = 0;
 	mem_used = false;
 	cout << "Akumulator jest pust" << endl;
@@ -40,38 +40,64 @@ void erase(){
 }
 
 void menu() {
-	int opcja;
-	cout << "Kalkulator" << endl;
-	cout << " uzyj + aby dodac" << endl;
-	cout << " uzyj - aby odjac" << endl;
-	cout << " uzyj * aby ponozyc" << endl;
-	cout << " uzyj / aby podzielic" << endl;
-	cout << " uzyj % aby uzyskac reszte" << endl;
-	cout << " napisz e aby wyczyscic" << endl;
-	cin >> opcja;
 
-	switch (opcja) {
+	double a, b;
 
-	case '+':
-		mem = add(a, b);
-		break;
-	case '-':
-		mem = sub(a, b);
-		break;
-	case '*':
-		mem = tim(a, b);
-	case '/':
-		mem = div(a, b);
-	case '%':
-		mem = mod(a, b);
+	char opcja;
+
+	while (true) {
+		cout << "Kalkulator" << endl;
+		cout << " uzyj + aby dodac" << endl;
+		cout << " uzyj - aby odjac" << endl;
+		cout << " uzyj * aby ponozyc" << endl;
+		cout << " uzyj / aby podzielic" << endl;
+		cout << " uzyj % aby uzyskac reszte" << endl;
+		cout << " napisz e aby wyczyscic" << endl;
 
 
+		cout << "Podaj pierwsza liczbe";
+		cin >> a;
+		cout << "Podaj opcje";
+		cin >> opcja;
+		cout << "Podaj druga liczbe";
+		cin >> b;
+
+		switch (opcja) {
+
+		case '+':
+			mem = add(a, b);
+			break;
+		case '-':
+			mem = sub(a, b);
+			break;
+		case '*':
+			mem = tim(a, b);
+			break;
+		case '/':
+			mem = div(a, b);
+			break;
+		case '%':
+			mem = mod(a, b);
+			break;
+		default:
+			cout << "???????" << endl;
+			continue;
+
+		}
+
+
+		mem_used = true;
+		cout << "Wynik: " << mem << endl;
 	}
+
 
 }
 
 
 int main()
 {
+
+	menu();
+	return 0;
 
 }
