70a154edddb69b0dc6dc767b20bf3c1d17a283e2
enrerst <287076@student.pwr.edu.pl>
2025-03-24 16:32:36 +0100

    Update kalkulator konsolowy.cpp

--- kalkulator konsolowy.cpp
+++ kalkulator konsolowy.cpp
@@ -1,78 +1,83 @@
-// kalkulator konsolowy.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+
 
 #include <iostream>
 using namespace std;
 
-double mem = 0;
 
-bool mem_used = false;
+class Kalkulator {
 
-double add(double a, double b) {
-	return a + b;
-}
+//tutaj dodac paradygmaty private
+	bool mem_used = false;
+	double mem;
 
-double sub(double a, double b) {
-	return a - b;
-}
-
-double tim(double a, double b) {
-	return a * b;
-}
-
-double div(double a, double b) {
-	if (b == 0) {
-		cout << "blad" << endl;
-		return 0;
+public:
+	
+	double add(double a, double b) {
+		return a + b;
 	}
-	return a / b;
-}
 
-double mod(double a, double b) {
-	return a - (int)(a / b) * b;
-}
+	double sub(double a, double b) {
+		return a - b;
+	}
 
-void erase() {
-	mem = 0;
-	mem_used = false;
-	cout << "Akumulator jest pust" << endl;
+	double tim(double a, double b) {
+		return a * b;
+	}
 
-}
-
-void menu() {
-
-	double a, b;
-
-	char opcja;
-
-	while (true) {
-		cout << "Kalkulator" << endl;
-		cout << " uzyj + aby dodac" << endl;
-		cout << " uzyj - aby odjac" << endl;
-		cout << " uzyj * aby ponozyc" << endl;
-		cout << " uzyj / aby podzielic" << endl;
-		cout << " uzyj % aby uzyskac reszte" << endl;
-		cout << " napisz e aby wyczyscic" << endl;
-		cout << " napisz q aby wyjsc" << endl;
-
-
-		cout << "Podaj pierwsza liczbe ";
-		cin >> a;
-		cout << "Podaj opcje ";
-		cin >> opcja;
-
-		if (opcja == 'e') {
-			erase();
+	double div(double a, double b) {
+		if (b == 0) {
+			cout << "blad" << endl;
+			return 0;
 		}
+		return a / b;
+	}
 
-		if (opcja == 'q') {
-			break;
-		}
+	double mod(double a, double b) {
+		return a - (int)(a / b) * b;
+	}
 
-		cout << "Podaj druga liczbe ";
-		cin >> b;
+	void erase() {
+		mem = 0;
+		mem_used = false;
+		cout << "Akumulator jest pust" << endl;
 
-		switch (opcja) {
+	};
+
+
+	void menu() {
+
+		double a, b;
+
+		char opcja;
+
+		while (true) {
+			cout << "Kalkulator" << endl;
+			cout << " uzyj + aby dodac" << endl;
+			cout << " uzyj - aby odjac" << endl;
+			cout << " uzyj * aby ponozyc" << endl;
+			cout << " uzyj / aby podzielic" << endl;
+			cout << " uzyj % aby uzyskac reszte" << endl;
+			cout << " napisz e aby wyczyscic" << endl;
+			cout << " napisz q aby wyjsc" << endl;
+
+
+			cout << "Podaj pierwsza liczbe ";
+			cin >> a;
+			cout << "Podaj opcje ";
+			cin >> opcja;
+
+			if (opcja == 'e') {
+				erase();
+			}
+
+			if (opcja == 'q') {
+				break;
+			}
+
+			cout << "Podaj druga liczbe ";
+			cin >> b;
+
+			switch (opcja) {
 
 			case '+':
 				mem = add(a, b);
@@ -93,21 +98,22 @@ void menu() {
 				cout << "???????" << endl;
 				continue;
 
+			}
+
+
+			mem_used = true;
+			cout << "Wynik: " << mem << endl;
 		}
 
 
-		mem_used = true;
-		cout << "Wynik: " << mem << endl;
-	}
-
-
-}
+	};
+};
 
 
 int main()
 {
-
-	menu();
+	Kalkulator Projekt;
+	Projekt.menu();
 	return 0;
 
 }
